8f6fe749de91ad38248d99f300daefe3d8cd985b
shokhrukh <114140248+Mrshokhrukh@users.noreply.github.com>
2024-06-04 16:06:36 +0500

    ready to upload

--- src/components/LoginWithField/LoginWithField.tsx
+++ src/components/LoginWithField/LoginWithField.tsx
@@ -7,7 +7,7 @@ type LoginWithFieldProps = {
 
 const LoginWithField: React.FC<LoginWithFieldProps> = ({ text, thumb }) => {
   return (
-    <button className="border-[0.5px] border-light-b flex items-center justify-center space-x-1.5 w-full h-[36px] rounded-[128px]">
+    <button className="border-[0.5px] border-light-b flex items-center justify-center space-x-1.5 w-full h-[36px] rounded-[128px] hover:border-x-btn-c bg-white hover:text-cyan-600">
       <img src={thumb} alt="" />
       <h2 className="text-sm font-semibold">{text}</h2>
     </button>
